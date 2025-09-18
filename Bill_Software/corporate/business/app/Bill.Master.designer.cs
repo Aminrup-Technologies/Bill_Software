d103cfa9d688eefb9fbde928fc22c08edda0a5b2
KK - TL <kaushikkumarmajhi.tml@gmail.com>
2025-09-18 17:40:16 +0530

    Added a page to attach the qtn/ po creation counts by date range.

--- Bill_Software/corporate/business/app/Bill.Master.designer.cs
+++ Bill_Software/corporate/business/app/Bill.Master.designer.cs
@@ -859,6 +859,15 @@ namespace Bill_Software.corporate.business.app {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl Purchess_due;
         
         /// <summary>
+        /// rpts_vw_qtn_po_counts control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl rpts_vw_qtn_po_counts;
+        
+        /// <summary>
         /// SalesTeam control.
         /// </summary>
         /// <remarks>
